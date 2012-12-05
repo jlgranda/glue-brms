97115c4f27c7d745bf78cb7893883850116c700a
cesar06ar <cesar@hp>
2012-12-05 12:11:17 -0500

    avance de admin de BussinesEntityType

--- src/main/java/org/eqaula/glue/service/BussinesEntityTypeList.java
+++ src/main/java/org/eqaula/glue/service/BussinesEntityTypeList.java
@@ -16,14 +16,19 @@
 package org.eqaula.glue.service;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.logging.Logger;
 import javax.enterprise.context.RequestScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import javax.inject.Named;
 import org.eqaula.glue.model.BussinesEntity;
 import org.eqaula.glue.model.BussinesEntityType;
+import org.eqaula.glue.util.QueryData;
+import org.eqaula.glue.util.QuerySortOrder;
 import org.primefaces.model.LazyDataModel;
 import org.primefaces.model.SortOrder;
 
@@ -39,7 +44,7 @@ public class BussinesEntityTypeList extends LazyDataModel<BussinesEntityType>{
     @Inject
     private Logger log;
     @Inject
-    private BussinesEntityService bussinesEntityService;
+    private BussinesEntityTypeService bussinesEntityTypeService;
     private List<BussinesEntityType> resultList;
     private int firstResult = 0;
     private BussinesEntityType[] selectedBussinesEntities;
@@ -57,36 +62,88 @@ public class BussinesEntityTypeList extends LazyDataModel<BussinesEntityType>{
         this.log = log;
     }
 
-    public BussinesEntityService getBussinesEntityService() {
-        return bussinesEntityService;
+    public BussinesEntityTypeService getBussinesEntityTypeService() {
+        return bussinesEntityTypeService;
     }
 
-    public void setBussinesEntityService(BussinesEntityService bussinesEntityService) {
-        this.bussinesEntityService = bussinesEntityService;
+    public void setBussinesEntityTypeService(BussinesEntityTypeService bussinesEntityTypeService) {
+        this.bussinesEntityTypeService = bussinesEntityTypeService;
     }
 
+    
     public List<BussinesEntityType> getResultList() {
         log.info("load BussinesEntitys");
         resultList = new ArrayList<BussinesEntityType>();
         if (resultList == null && getSelectedBussinesEntityType() != null) {
             //resultList = bussinesEntityService.find(this.getPageSize(), firstResult, getSelectedBussinesEntityType());
+            resultList = bussinesEntityTypeService.find(this.getPageSize(), firstResult);            
         }
         
         return resultList;
     }
+    
+    public String bind() {
+        resultList = bussinesEntityTypeService.findAll();
+        String summary = "Encontrados!";
+        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, summary, null));
+        return "/pages/admin/bussinesentitytype/list";
+    }
+    
+    public int getNextFirstResult() {
+        return firstResult + this.getPageSize();
+    }
 
+    public int getPreviousFirstResult() {
+        return this.getPageSize() >= firstResult ? 0 : firstResult - this.getPageSize();
+    }
+    
     public void setResultList(List<BussinesEntityType> resultList) {
         this.resultList = resultList;
     }
-
+           
     public int getFirstResult() {
         return firstResult;
     }
 
     public void setFirstResult(int firstResult) {
+        log.info("set first result + firstResult");
         this.firstResult = firstResult;
+        this.resultList = null;
+    }
+    
+    public boolean isPreviousExists() {
+        return firstResult > 0;
     }
 
+    @Override
+    public BussinesEntityType getRowData(String rowKey) {
+
+        return bussinesEntityTypeService.findByName(rowKey);
+    }
+
+    @Override
+    public Object getRowKey(BussinesEntityType entity) {
+        return entity.getName();
+    }
+
+    @Override
+    public List<BussinesEntityType> load(int first, int pageSize, String sortField, SortOrder sortOrder, Map<String, String> filters) {
+
+        int end = first + pageSize;
+
+        QuerySortOrder order = QuerySortOrder.ASC;
+        if (sortOrder == SortOrder.DESCENDING) {
+            order = QuerySortOrder.DESC;
+        }
+        Map<String, Object> _filters = new HashMap<String, Object>();
+        /*_filters.put(BussinesEntity_.parent.getName(), getSelectedAccount()); //Filtro por defecto
+        _filters.putAll(filters);*/
+
+        QueryData<BussinesEntityType> qData = (QueryData<BussinesEntityType>) bussinesEntityTypeService.find(first, end);
+        this.setRowCount(qData.getTotalResultCount().intValue());
+        return qData.getResult();
+    }
+    
     public BussinesEntityType[] getSelectedBussinesEntities() {
         return selectedBussinesEntities;
     }
@@ -104,9 +161,9 @@ public class BussinesEntityTypeList extends LazyDataModel<BussinesEntityType>{
     }
 
     
-    @Override
-    public List<BussinesEntityType> load(int first, int pageSize, String sortField, SortOrder sortOrder, Map<String, String> filters) {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
+//    @Override
+//    public List<BussinesEntityType> load(int first, int pageSize, String sortField, SortOrder sortOrder, Map<String, String> filters) {
+//        throw new UnsupportedOperationException("Not supported yet.");
+//    }
 
 }
---
+++ src/main/java/org/eqaula/glue/service/BussinesEntityTypeService.java
@@ -0,0 +1,118 @@
+/*
+ * Copyright 2012 cesar.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.eqaula.glue.service;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Logger;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import org.eqaula.glue.model.BussinesEntity;
+import org.eqaula.glue.model.BussinesEntityAttribute;
+import org.eqaula.glue.model.BussinesEntityType;
+import org.eqaula.glue.model.BussinesEntityType_;
+import org.eqaula.glue.model.BussinesEntity_;
+import org.eqaula.glue.model.GroupType;
+import org.eqaula.glue.model.Property;
+import org.eqaula.glue.util.PersistenceUtil;
+import org.eqaula.glue.util.QueryData;
+import org.eqaula.glue.util.QuerySortOrder;
+
+/**
+ *
+ * @author cesar
+ */
+public class BussinesEntityTypeService extends PersistenceUtil<BussinesEntity> {
+
+    private static final long serialVersionUID = 6569835981443699931L;
+    @Inject
+    private Logger log;
+
+    public BussinesEntityTypeService() {
+        super(BussinesEntity.class);
+    }
+    
+    @Override
+    public void setEntityManager(EntityManager em) {
+        this.em = em;
+    }
+    
+    //metodo buscar por nombre
+    public BussinesEntityType findByName(final String name) {
+
+        log.info("find BussinesEntityType with name " + name);
+
+        CriteriaBuilder builder = getCriteriaBuilder();
+        CriteriaQuery<BussinesEntityType> query = builder.createQuery(BussinesEntityType.class);
+
+        Root<BussinesEntityType> bussinesEntityType = query.from(BussinesEntityType.class);
+
+        query.where(builder.equal(bussinesEntityType.get(BussinesEntityType_.name), name));
+
+        return getSingleResult(query);
+    }
+    
+    //metodo buscar  lista de BussinesEntityType
+     public List<BussinesEntityType> find(int maxresults, int firstresult) {
+        log.info("find BussinesEntityType, max results " + maxresults + " next result " + firstresult);
+
+        CriteriaBuilder builder = getCriteriaBuilder();
+        CriteriaQuery<BussinesEntityType> query = builder.createQuery(BussinesEntityType.class);
+
+        Root<BussinesEntityType> from = query.from(BussinesEntityType.class);
+        query.select(from).orderBy(builder.desc(from.get(BussinesEntityType_.name)));
+        return getResultList(query, maxresults, firstresult);
+    }
+    
+    //metodo count
+    public Long count() {
+        CriteriaBuilder builder = getCriteriaBuilder();
+        CriteriaQuery<Long> query = builder.createQuery(Long.class);
+        Root<BussinesEntityType> from = query.from(BussinesEntityType.class);
+
+        query.select(builder.count(from)); 
+        return this.<Long>getTypedSingleResult(query); 
+    } 
+     
+    //metodo saveAtributes 
+ /*   public void saveAttributes(BussinesEntity bussinesEntity, Map<Property, Object> attributes) {
+        BussinesEntityAttribute attribute = null;
+        for (Map.Entry<Property, Object> e : attributes.entrySet()) {
+            attribute = new BussinesEntityAttribute();
+            attribute.setName(e.getKey().getName());
+            attribute.setValue((Serializable) e.getValue());
+            attribute.setType(e.getKey().getType());
+            bussinesEntity.addBussinesEntityAttribute(attribute);
+        }
+
+        super.save(bussinesEntity);
+        em.flush();
+    }*/
+    
+    public List<BussinesEntityType> findAll() {
+        Query query = em.createNamedQuery("BussinesEntityType.findAllBussinesEntityTypes");         
+        return query.getResultList();
+    }
+    
+    //metodo para buscar por estructura
+    
+}
