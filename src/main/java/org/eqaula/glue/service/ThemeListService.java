596b3f1a16a16448ed8b83c55abb26cc0f684486
dianita <dianita@dianita-SVT13115FLS>
2013-03-14 08:51:26 -0500

    Cambios en CRUD BalancedScoreCard

---
+++ src/main/java/org/eqaula/glue/service/ThemeListService.java
@@ -0,0 +1,141 @@
+/*
+ * Copyright 2013 dianita.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.eqaula.glue.service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.RequestScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.persistence.EntityManager;
+import org.eqaula.glue.cdi.Web;
+import org.eqaula.glue.model.management.BalancedScorecard;
+import org.eqaula.glue.model.management.Theme;
+import org.eqaula.glue.util.QueryData;
+import org.eqaula.glue.util.QuerySortOrder;
+import org.eqaula.glue.util.UI;
+import org.primefaces.event.SelectEvent;
+import org.primefaces.event.UnselectEvent;
+import org.primefaces.model.LazyDataModel;
+import org.primefaces.model.SortOrder;
+
+/*
+ * @author dianita
+ */
+
+@Named
+@RequestScoped
+public class ThemeListService  extends  LazyDataModel<Theme>{
+    private static final int MAX_RESULTS = 5;
+    private static org.jboss.solder.logging.Logger log = org.jboss.solder.logging.Logger.getLogger(ThemeListService.class);
+    private static final long serialVersionUID = -3077669958110754391L;
+    
+    @Inject
+    @Web
+    private EntityManager entityManager;
+    @Inject
+    private ThemeService themeService;
+    private List<Theme> resultList;
+    private int firstResult = 0;
+    private Theme[] selectedThemes;
+    private Theme selectedTheme;
+
+    public ThemeListService() {
+       setPageSize(MAX_RESULTS);
+       resultList= new ArrayList<Theme>();
+    }
+
+    public List<Theme> getResultList() {
+        if(resultList.isEmpty()){
+            resultList= themeService.getThemes();
+        }
+        return resultList;
+    }
+
+    public void setResultList(List<Theme> resultList) {
+        this.resultList = resultList;
+    }
+
+    public int getFirstResult() {
+        return firstResult;
+    }
+
+    public void setFirstResult(int firstResult) {
+        this.firstResult = firstResult;
+        this.resultList= null;
+    }
+
+    public Theme getSelectedTheme() {
+        return selectedTheme;
+    }
+
+    public void setSelectedTheme(Theme selectedTheme) {
+        this.selectedTheme = selectedTheme;
+    }
+    
+     public int getNextFirstResult() {
+        return firstResult + this.getPageSize();
+    }
+
+    public int getPreviousFirstResult() {
+        return this.getPageSize() >= firstResult ? 0 : firstResult - this.getPageSize();
+    }
+
+     @Override
+    public List<Theme> load(int first, int pageSize, String sortField, SortOrder sortOrder, Map<String, String> filters) {
+        int end = first + pageSize;
+         QuerySortOrder order = QuerySortOrder.ASC;
+        if (sortOrder == SortOrder.DESCENDING) {
+            order = QuerySortOrder.DESC;
+        }
+        Map<String, Object> _filters = new HashMap<String, Object>();
+         QueryData<Theme> qData = themeService.find(first, end, sortField, order, _filters);
+        this.setRowCount(qData.getTotalResultCount().intValue());
+        return qData.getResult();
+    }
+    
+     @PostConstruct
+    public void init() {
+        themeService.setEntityManager(entityManager);
+    }
+     
+    public void onRowSelect(SelectEvent event) {
+        FacesMessage msg = new FacesMessage(UI.getMessages("module.theme") + " " + UI.getMessages("common.selected") , ((Theme) event.getObject()).getName());
+        FacesContext.getCurrentInstance().addMessage(null, msg);
+    }
+
+    public void onRowUnselect(UnselectEvent event) {
+        FacesMessage msg = new FacesMessage(UI.getMessages("module.theme") + " " + UI.getMessages("common.unselected") , ((Theme) event.getObject()).getName());
+        FacesContext.getCurrentInstance().addMessage(null, msg);
+        this.setSelectedTheme(null);
+    }
+    
+    @Override
+    public Theme getRowData(String rowKey) {
+        return themeService.findByName(rowKey);
+    }
+
+    @Override
+    public Object getRowKey(Theme entity) {
+        return entity.getName();
+    }
+    
+}
--- src/main/java/org/eqaula/glue/service/ThemeService.java
+++ src/main/java/org/eqaula/glue/service/ThemeService.java
@@ -15,10 +15,82 @@
  */
 package org.eqaula.glue.service;
 
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import org.eqaula.glue.model.management.Theme;
+import org.eqaula.glue.model.management.Theme_;
+import org.eqaula.glue.model.profile.Profile;
+import org.eqaula.glue.util.PersistenceUtil;
+
 /**
  *
  * @author dianita
  */
-public class ThemeService {
+public class ThemeService extends PersistenceUtil<Theme> implements Serializable{
+    private static org.jboss.solder.logging.Logger log = org.jboss.solder.logging.Logger.getLogger(BussinesEntityService.class);
+    private static final long serialVersionUID = -3246755068687015421L;
+
+    public ThemeService() {
+        super(Theme.class);
+    }
     
+    @Override
+    public void setEntityManager(EntityManager em){
+        this.em=em;
+    }
+    
+    public Long count(){
+        return count(Theme.class);
+    }
+    
+    public List<Theme> getThemes() {
+        return findAll(Theme.class);
+    }
+
+    public List<Theme> getThemes(final int limit, final int offset) {
+        return findAll(Theme.class);
+    }
+
+    public Theme getThemeById(final Long id) {
+        return (Theme) findById(Theme.class, id);
+    }
+    
+    public Theme findByName(final String name) {
+        log.info("find Theme with name " + name);
+        CriteriaBuilder builder = getCriteriaBuilder();
+        CriteriaQuery<Theme> query = builder.createQuery(Theme.class);
+        Root<Theme> bussinesEntityType = query.from(Theme.class);
+        query.where(builder.equal(bussinesEntityType.get(Theme_.name), name));
+        return getSingleResult(query);
+    }
+
+    public List<Theme> findByProfile(Profile profile) {
+        log.info("find Theme with profile " + profile);
+        if (profile == null) return new ArrayList<Theme>(0);
+
+        CriteriaBuilder builder = getCriteriaBuilder();
+        CriteriaQuery<Theme> query = builder.createQuery(Theme.class);
+        Root<Theme> bussinesEntityType = query.from(Theme.class);
+        query.where(builder.equal(bussinesEntityType.get(Theme_.author), profile));
+        return getResultList(query);
+    }
+
+    public Theme findByProfileAndCode(Profile profile, String themeCode) {
+        log.info("find Theme with profile " + profile + " theme code " + themeCode);
+        CriteriaBuilder builder = getCriteriaBuilder();
+        CriteriaQuery<Theme> query = builder.createQuery(Theme.class);
+        Root<Theme> bussinesEntityType = query.from(Theme.class);
+        query.where(builder.equal(bussinesEntityType.get(Theme_.author), profile));
+        return getSingleResult(query);
+    }
+
+    public void create(Profile loggedIn, Theme current) {
+        current.setAuthor(loggedIn);
+        create(current);
+    }
 }
